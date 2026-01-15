75935d6b186f3e6dae0313eb9b1c8c2754b795e0
i1SaSa <s9ny398335@gmail.com>
2026-01-15 19:17:54 +0300

    Add: implement combination printing logic in 6-print_comb3.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+x = 0
+for i in range(10):
+    x += 1
+    for j in range(x, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j), end="")
+        else:
+            print("{}{}, ".format(i, j), end="")
