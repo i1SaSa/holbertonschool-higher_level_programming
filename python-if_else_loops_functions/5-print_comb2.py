b6871689ccf4edc0271179c6abb5fdbbc28b58c3
i1SaSa <s9ny398335@gmail.com>
2026-01-15 17:07:31 +0300

    Fix: update print logic in 5-print_comb2.py for correct formatting of two-digit combinations

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(100):
     print("{}".format(i // 10), end="")
-    print("{}, ".format(i % 10), end="")
+    if i != 99:
+        print("{}, ".format(i % 10), end="")
+    else:
+        print("{} ".format(i % 10))
