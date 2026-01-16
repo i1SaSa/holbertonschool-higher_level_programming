10a6079dbcd9e404c2775db70b9ec1cb981255a5
i1SaSa <s9ny398335@gmail.com>
2026-01-16 20:53:38 +0300

    Fix: refactor pow function to handle negative exponents correctly in 11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
-    x = a
-    for i in range(b - 1):
-        a *= x
+    if b > 0:
+        x = a
+        for i in range(b - 1):
+            a *= x
+    else:
+        x = a
+        for i in range(b - 1):
+            a *= x
+        return 1/a
     return a
