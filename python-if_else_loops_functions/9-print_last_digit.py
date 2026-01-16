13c7cf4f38a5c4f3311bff41e2214260aaa35744
i1SaSa <s9ny398335@gmail.com>
2026-01-16 21:09:15 +0300

    Fix: update print_last_digit function to print last digit instead of returning it in 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return 10 - (number % 10)
-    return number % 10
+        return print(10 - (number % 10))
+    return print(number % 10)
