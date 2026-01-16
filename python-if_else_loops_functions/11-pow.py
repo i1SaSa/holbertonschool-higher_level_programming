78f230caab09aefe30edacf54b89a2a312be4c0a
i1SaSa <s9ny398335@gmail.com>
2026-01-16 21:01:54 +0300

    Fix: refactor pow function to improve readability and correct loop logic in 11-pow.py

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def pow(a, b):
+    x = a
     if b > 0:
-        x = a
         for i in range(b - 1):
             a *= x
+        return a
     else:
-        x = a
-        for i in range(b - 1):
+        for i in range(0, b + 1, -1):
             a *= x
         return 1/a
     return a
