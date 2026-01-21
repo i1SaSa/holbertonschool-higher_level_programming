fb4e85ae6d9eb6fb5ef2f0a3d311f132f39606cb
i1SaSa <s9ny398335@gmail.com>
2026-01-21 18:33:26 +0300

    Fix: correct matrix printing logic in print_matrix_integer function

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        print("\n")
+        sep = ""
         for g in i:
-            print("{:d}".format(g), end=" ")
+            print(sep + "{:d}".format(g), end="")
+            sep = " "
+    print()
