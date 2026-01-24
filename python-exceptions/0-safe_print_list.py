59a75a827ed31d2ea3d3b494374366f533d5b744
i1SaSa <s9ny398335@gmail.com>
2026-01-24 21:18:59 +0300

    Refactor: replace square_matrix_simple function with safe_print_list to print elements of a list safely

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    new_matrix = list(map(lambda x: list(map(lambda y: y * y, x)), matrix))
-    return new_matrix
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            break
+    print()
+    return count
