3d0e4ef8b975c113a76c521afcb08153edff9b77
i1SaSa <s9ny398335@gmail.com>
2026-01-26 16:45:06 +0300

    Add: create square class with a docstring and _size method

---
+++ python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''
+Docstring for python-classes.1-square
+'''
+
+
+class square:
+    '''
+    Docstring for square
+    '''
+    def _size():
+        '''
+                Docstring for _size
+                '''
+        pass
