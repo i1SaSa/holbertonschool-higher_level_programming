8b8c8110147d272a3b2b11424f0856f4095b9f4a
i1SaSa <s9ny398335@gmail.com>
2026-01-27 07:27:57 +0300

    Add: enhance docstrings for __init__, area, size, position, and my_print methods

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -10,18 +10,41 @@ class Square:
     '''
 
     def __init__(self, size=0, position=(0, 0)):
+        '''
+                Docstring for __init__
+
+                :param self: Description
+                :param size: Description
+                :param position: Description
+                '''
         self.__size = size
         self.__position = position
 
     def area(self):
+        '''
+                Docstring for area
+
+                :param self: Description
+                '''
         return self.__size * self.__size
 
     @property
     def size(self):
+        '''
+                Docstring for size
+
+                :param self: Description
+                '''
         return self.__size
 
     @size.setter
     def size(self, value):
+        '''
+                Docstring for size
+
+                :param self: Description
+                :param value: Description
+                '''
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -30,14 +53,30 @@ class Square:
 
     @property
     def position(self):
+        '''
+                Docstring for position
+
+                :param self: Description
+                '''
         return self.__position
 
     @position.setter
     def position(self, value):
+        '''
+                Docstring for position
+
+                :param self: Description
+                :param value: Description
+                '''
         if not isinstance(value, tuple) and all(n < 0 for n in value) and len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def my_print(self):
+        '''
+                Docstring for my_print
+
+                :param self: Description
+                '''
         if self.__size == 0:
             print()
             return
