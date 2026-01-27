4d1812069e9d0516b1265ad776bddd44331df13f
i1SaSa <s9ny398335@gmail.com>
2026-01-27 06:49:12 +0300

    Add: implement Square class with __init__ and area methods

---
+++ python-classes/4-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''
+Docstring for python-classes.3-square
+'''
+
+
+class Square:
+    '''
+    Docstring for Square
+    '''
+
+    def __init__(self, size=0):
+        '''
+        Docstring for __init__
+
+        :param self: Description
+        :param size: Description
+        '''
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        '''
+                Docstring for area
+
+                :param self: Description
+                '''
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
