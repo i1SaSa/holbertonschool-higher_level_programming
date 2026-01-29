091ccfbdd78f62bd94489749a8a58926f3750399
i1SaSa <s9ny398335@gmail.com>
2026-01-29 19:55:40 +0300

    Add: implement Rectangle class with properties, area, perimeter, and string representation methods

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+'''
+    Docstring for python-more_classes.1-rectangle
+    '''
+
+
+class Rectangle:
+    '''
+        Docstring for Rectangle
+        '''
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2*(self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        one_row = "#" * self.__width
+        rows_list = [one_row] * self.__height
+        return "\n".join(rows_list)
+
+    def __str__(self):
+        return "Rectangle({},{})".format(self.width, self.height)
