157b2fde94ec3fee1a9f18e6ab728b11289daa1f
i1SaSa <s9ny398335@gmail.com>
2026-01-30 22:46:04 +0300

    Add: enhance docstrings for the lookup function and module

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+'''
+Docstring for python-inheritance.0-lookup
+'''
+
+
 def lookup(obj):
+    '''
+        Docstring for lookup
+
+        :param obj: Description
+        '''
     return dir(obj)
