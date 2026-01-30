8940ceba08d41588e1a2fc8f6eb957aa7ff5f87f
i1SaSa <s9ny398335@gmail.com>
2026-01-30 23:16:33 +0300

    Add: implement MyList class with print_sorted method and corresponding tests

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Docstring for python-inheritance.1-my_list
+'''
+
+
+class MyList(list):
+    '''
+        Docstring for MyList
+        '''
+
+    def print_sorted(self):
+        print(sorted(self))
