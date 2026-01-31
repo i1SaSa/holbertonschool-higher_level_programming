406bdbc736352439e4c8f95c6259b0b2895b6c47
i1SaSa <s9ny398335@gmail.com>
2026-01-31 12:22:49 +0300

    Add: implement is_same_class function with initial docstring

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Docstring for python-inheritance.2-is_same_class
+'''
+
+
+def is_same_class(obj, a_class):
+    '''
+        Docstring for is_same_class
+
+        :param obj: Description
+        :param a_class: Description
+        '''
+    return type(obj) is a_class
