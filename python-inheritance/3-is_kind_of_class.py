068b1542c92376ab4c6b425d6e9406ab9947fbbb
i1SaSa <s9ny398335@gmail.com>
2026-01-31 12:24:51 +0300

    Add: implement is_kind_of_class function with initial docstring

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Docstring for python-inheritance.3-is_kind_of_class
+'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''
+        Docstring for is_kind_of_class
+
+        :param obj: Description
+        :param a_class: Description
+        '''
+    return isinstance(obj, a_class)
