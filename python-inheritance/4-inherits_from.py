1cb3222f1f69731878ac70f1599d184d30a8a0b3
i1SaSa <s9ny398335@gmail.com>
2026-01-31 12:47:18 +0300

    Add: implement inherits_from function with initial docstring

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+Docstring for python-inheritance.4-inherits_from
+'''
+
+
+def inherits_from(obj, a_class):
+    '''
+        Docstring for inherits_from
+
+        :param obj: Description
+        :param a_class: Description
+        '''
+    return isinstance(obj, a_class)
