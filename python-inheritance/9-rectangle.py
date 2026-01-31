a2686ab30c6695ceae0a22d5f9dbebeac441a3ca
i1SaSa <s9ny398335@gmail.com>
2026-01-31 15:06:19 +0300

    Add: create Rectangle class inheriting from BaseGeometry with area and string representation methods

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Docstring for python-inheritance.8-rectangle
+'''
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''
+        Docstring for Rectangle
+        '''
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__height * self.__width
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
