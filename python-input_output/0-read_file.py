790a86628b7f94b837508f24163ad088ef6afa49
i1SaSa <s9ny398335@gmail.com>
2026-02-04 14:56:32 +0300

    Refactor: update read_file function to improve output formatting

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,14 +2,13 @@
 '''
 Docstring for python-input_output.0-read_file
 '''
+
+
 def read_file(filename=""):
-	'''
-	Docstring for read_file
-	
-	:param filename: Description
-	'''
-	with open(filename,"r") as f:
-		print(f.read())
+    '''
+    Docstring for read_file
 
-
-
+    :param filename: Description
+    '''
+    with open(filename, "r") as f:
+        print(f.read(),end="")
