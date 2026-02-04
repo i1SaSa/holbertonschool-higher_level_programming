37d7524257626adbe1cdcafbc85ed8f7ea3d5733
i1SaSa <s9ny398335@gmail.com>
2026-02-04 14:53:11 +0300

    Add: enhance docstrings for read_file function and module

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+'''
+Docstring for python-input_output.0-read_file
+'''
 def read_file(filename=""):
+	'''
+	Docstring for read_file
+	
+	:param filename: Description
+	'''
 	with open(filename,"r") as f:
 		print(f.read())
 
