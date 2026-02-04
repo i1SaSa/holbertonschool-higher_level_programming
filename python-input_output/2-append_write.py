5fc49670d154a44af0708d4d1c78e9c88fc8d97e
i1SaSa <s9ny398335@gmail.com>
2026-02-04 15:54:40 +0300

    Add: enhance docstrings for append_write function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+'''
+Docstring for python-input_output.2-append_write
+'''
+
+
 def append_write(filename="", text=""):
+    '''
+        Docstring for append_write
+
+        :param filename: Description
+        :param text: Description
+        '''
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
