6e284a58cb96040dec4443944ce68ca4dbceb17a
i1SaSa <s9ny398335@gmail.com>
2026-02-04 18:00:39 +0300

    Add: implement to_json_string function to convert an object to JSON string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import json
+'''
+Docstring for python-input_output.3-to_json_string
+'''
+
+
+def to_json_string(my_obj):
+    '''
+    Docstring for to_json_string
+
+    :param my_obj: Description
+    '''
+    return json.dumps(my_obj)
