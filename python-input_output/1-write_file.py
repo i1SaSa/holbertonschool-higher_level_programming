4f1bf36ab9bf71fca9d7c9b4e743dee6925ff5b6
i1SaSa <s9ny398335@gmail.com>
2026-02-04 15:13:59 +0300

    Add: enhance docstrings for write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+'''
+Docstring for python-input_output.1-write_file
+'''
+
+
 def write_file(filename="", text=""):
+    '''
+        Docstring for write_file
+
+        :param filename: Description
+        :param text: Description
+        '''
     with open(filename, "w", encoding="UTF8") as f:
         f.write(text)
