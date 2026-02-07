5cbbaf7c6ad901983fa2acf1144dcf93d24d8197
i1SaSa <s9ny398335@gmail.com>
2026-02-07 11:47:53 +0300

    Fix: refactor 7-add_item.py to properly load and save command line arguments to JSON file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,6 +9,11 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-load_from_json_file(add_item.json)
-for i in sys.argv:
-    save_to_json_file(i, add_item.json)
+try:
+    items = load_from_json_file("add_item.json")
+except Exception:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, "add_item.json")
