bf32685ac578d01f49cdccdc09006541b77bf6fa
i1SaSa <s9ny398335@gmail.com>
2026-02-07 18:56:39 +0300

    Add: implement class_to_json function to serialize class instances to a dictionary

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.8-class_to_json
+'''
+
+
+def class_to_json(obj):
+    '''
+        Docstring for class_to_json
+
+        :param obj: instant of class
+        '''
+    return obj.__dict__
