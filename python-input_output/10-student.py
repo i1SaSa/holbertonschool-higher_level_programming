a0cdaa53c43f7405c56ac5147a9f85a670c7f4af
i1SaSa <s9ny398335@gmail.com>
2026-02-07 21:15:53 +0300

    Add: create Student class with initialization and to_json method

---
+++ python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''
+Docstring for python-input_output.9-student
+'''
+
+
+class Student():
+    '''
+    Docstring for Student
+    '''
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            res = {}
+            for k in attrs:
+                if k in self.__dict__:
+                    res[k] = self.__dict__[k]
+            return res
+        return self.__dict__
