02a782830a80d0b4d7fdbe7a08c062b0d5745742
i1SaSa <s9ny398335@gmail.com>
2026-03-22 22:30:42 +0300

    Add: create SQL query to list all cities by state

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all cities contained in the database hbtn_0d_usa
+-- Results are sorted in ascending order by cities.id
+-- Uses only one SELECT statement
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
