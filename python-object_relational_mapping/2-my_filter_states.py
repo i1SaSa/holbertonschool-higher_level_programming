8c8ef5a0d6c47d9425cee4bd7c06d34586dd3a5f
i1SaSa <s9ny398335@gmail.com>
2026-03-23 23:18:12 +0300

    Add: implement script to list states matching a given name from the database

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Lists all values in the states table where name matches the argument """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(
+        state_name_searched)
+    cursor.execute(query)
+
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cursor.close()
+    db.close()
