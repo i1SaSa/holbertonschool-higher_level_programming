fb627c7782629ffb9f1b6cf14044268ba5edb16d
i1SaSa <s9ny398335@gmail.com>
2026-03-25 17:56:33 +0300

    Refactor: update script to add State object 'Louisiana' to the database

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""
-Prints the State object with the name passed as argument
-"""
+""" Adds the State object 'Louisiana' to the database """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    state_to_find = sys.argv[4]
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            user, passwd, db_name),
-        pool_pre_ping=True
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
+
     new_state = State(name="Louisiana")
 
     session.add(new_state)
