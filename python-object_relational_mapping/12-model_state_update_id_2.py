b24663afc523c9180d6916ff4b364e43c62d4185
i1SaSa <s9ny398335@gmail.com>
2026-03-25 18:03:06 +0300

    Refactor: update script to change the name of a State object in the database

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument
+Changes the name of a State object from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
@@ -8,23 +8,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    state_to_find = sys.argv[4]
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            user, passwd, db_name),
-        pool_pre_ping=True
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
     )
 
     Session = sessionmaker(bind=engine)
     session = Session()
+
     state_to_update = session.query(State).filter(State.id == 2).first()
 
     if state_to_update:
         state_to_update.name = "New Mexico"
-        session.commit()  
+        session.commit()
 
-        session.close()
+    # إغلاق الجلسة
+    session.close()
