cb5f1c4cca6995b623e839a360558d866169be6c
i1SaSa <s9ny398335@gmail.com>
2026-03-25 17:52:06 +0300

    Add: create script to print the State object by name from the database

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Prints the State object with the name passed as argument
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db_name = sys.argv[3]
+    state_to_find = sys.argv[4]
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            user, passwd, db_name),
+        pool_pre_ping=True
+    )
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(State.name == state_to_find).first()
+
+    if state:
+        print("{}".format(state.id))
+    else:
+        print("Not found")
+
+    session.close()
