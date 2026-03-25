afb93f7a426609e9d24c70c9debcedcfd2a44657
i1SaSa <s9ny398335@gmail.com>
2026-03-25 18:04:49 +0300

    Refactor: update docstring and streamline code for deleting State objects

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument
+Deletes all State objects with a name containing the letter a
+from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
@@ -8,25 +9,20 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db_name = sys.argv[3]
-    state_to_find = sys.argv[4]
-
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            user, passwd, db_name),
-        pool_pre_ping=True
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ), pool_pre_ping=True
     )
-
+    
     Session = sessionmaker(bind=engine)
     session = Session()
-    states_to_delete = session.query(State).filter(
-        State.name.contains('a')).all()
+
+    states_to_delete = session.query(State).filter(State.name.contains('a')).all()
 
     for state in states_to_delete:
         session.delete(state)
 
-        session.commit()
+    session.commit()
 
-        session.close()
+    session.close()
